25302ad540ff22be551ebc14f1212487e1c6cd6b
Julie Behr <julie.behr@choderalab.org>
2015-09-22 11:08:49 -0400

    initial commit

---
+++ pdbfix-1OL5-1OL7.py
@@ -0,0 +1,36 @@
+from pdbfixer import PDBFixer
+from simtk.openmm.app import PDBFile
+
+RESIDUES_TO_REMOVE = ['SO4']
+
+def delete_this_line(line):
+    for resi in RESIDUES_TO_REMOVE:
+        if line.find(resi)!=-1:
+            return True
+    return False
+
+for pdbid in ['1OL5','1OL7']:
+
+    fixer = PDBFixer(pdbid=pdbid)
+
+#    fixer.findMissingResidues()
+    fixer.missingResidues = {}
+    fixer.findMissingAtoms()
+    fixer.addMissingAtoms()
+
+    fixer.findNonstandardResidues()
+    fixer.replaceNonstandardResidues()
+    fixer.addMissingHydrogens(pH=7.4)
+
+    PDBFile.writeFile(fixer.topology, fixer.positions, open(pdbid+'-WT-int-pdbfixer.pdb', 'w'))
+
+    with open(pdbid+'-WT-int-pdbfixer.pdb', 'r') as fold:
+        with open(pdbid+'-WT-pdbfixer.pdb', 'w') as fnew:
+            for line in fold.readlines():
+                if not delete_this_line(line):
+                    fnew.write(line)
+
+
+
+
+
