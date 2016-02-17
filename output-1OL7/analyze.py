c3f330cd691d2cc1c7dc709f97227b4ab60d14e8
juliebehr <jul.marie.behr@gmail.com>
2016-02-17 13:22:43 -0500

    Using wernet_nilsson hbonds

--- output-1OL7/analyze.py
+++ output-1OL7/analyze.py
@@ -21,30 +21,26 @@ for index in range(5):
 
         distances, residue_pairs = md.compute_contacts(traj, contacts=[[e181.index,q185.index]])
         distances_to_plot = [d[0] for d in distances]
-        print(len(distances))
         plt.plot(distances)
 
         res185atoms = [atom.index for atom in res185.atoms]
         haystack = traj.top.select("water")
         neighbors = md.compute_neighbors(traj, 0.6, res185atoms, haystack_indices=haystack)
 
-        print(res185atoms)
-        print(len(neighbors[0]))
         neighbor_set = set(chain.from_iterable(neighbors))
         neighbor_set = list(neighbor_set)
-        hbonds0 = md.baker_hubbard(traj, exclude_water=False, proposed_donor_indices=res185atoms, proposed_acceptor_indices=neighbor_set)
-        print(hbonds0)
-        hbonds1 = md.baker_hubbard(traj, exclude_water=False, proposed_donor_indices=neighbor_set, proposed_acceptor_indices=res185atoms)
-        print(hbonds1)
-        if hbonds0.shape != (0,3) and hbonds1.shape != (0,3):
-            hbonds = np.array(hbonds0 + hbonds1)
-        elif hbonds0.shape == (0,3):
-            hbonds = hbonds1
-        else:
-            hbonds = hbonds0
+        # using wernet_nilsson because the output is hbonds per frame
+        hbonds0 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=res185atoms, proposed_acceptor_indices=neighbor_set)
+        hbonds1 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=neighbor_set, proposed_acceptor_indices=res185atoms)
+
+        # this is wrong now -- want to match up corresponding frames!
+        hbonds = np.array(hbonds0 + hbonds1)
 
         label = lambda hbond : '%s -- %s' % (traj.topology.atom(hbond[0]), traj.topology.atom(hbond[2]))
-        for hbond in hbonds:
+        for index, frame in enumerate(hbonds):
+            if len(frame) > 0:
+                break
+        for hbond in hbonds[index]:
             print label(hbond)
 
         # make plots of all data past t = 250ns
