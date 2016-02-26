3ad4a228e2b9db1ea61c7de1d0e89007311b2b14
juliebehr <jul.marie.behr@gmail.com>
2016-02-26 00:25:14 -0500

    Preparing salt bridge plot generating script for multiple SBs

--- salt_bridge_2dhists.py
+++ salt_bridge_2dhists.py
@@ -1,10 +1,7 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import mdtraj as md
 import numpy as np
-from msmbuilder import dataset
-from itertools import chain
 import sys
 import math
 from matplotlib.pyplot import cm
@@ -25,22 +22,8 @@ projects = ['11410','11411']
 project_dirs = {'11410':'./output-1OL5','11411':'./output-1OL7'}
 system = {'11410':'with TPX2','11411':'without TPX2'}
 
-colors = [
-    [{'fill':'#FFB5C5','line':'#8B0A50'},  # pink 1, deeppink 4
-     {'fill':'#BCD2EE','line':'#000080'}], # lightsteelblue 2, navy*
-    [{'fill':'#E066FF','line':'#551A8B'},  # mediumorchid 1, purple 4
-     {'fill':'#C1FFC1','line':'#006400'}], # darkseagreen 1, darkgreen
-    [{'fill':'#98F5FF','line':'#00688B'},  # cadetblue 1, deepskyblue 4
-     {'fill':'#FFE4B5','line':'#9C661F'}], # moccasin, brick
-    [{'fill':'#A2CD5A','line':'#556B2F'},  # darkolivegreen 3, darkolivegreen
-     {'fill':'#FF7F24','line':'#FF4500'}], # chocolate 1, orangered 1
-    [{'fill':'#FFF68F','line':'#8B7500'},  # khaki 1, gold 4
-     {'fill':'#F08080','line':'#EE0000'}]  # lightcoral, red 2
-]
-
 with open('./output-1OL7/run-index.txt','r') as fi:
     run_index = fi.read()
-
 mutant = dict()
 for entry in run_index.split('\n'):
     try:
@@ -48,33 +31,34 @@ for entry in run_index.split('\n'):
     except:
         pass
 
-time_x = np.arange(0,2000)*0.25 # output written every 250 ps
+def plot_2dhist(bridge, x_axis, minimum_distance, run, project):
+    fig1 = plt.figure()
+    plt.hist2d(x_axis[minimum_distance > 0],minimum_distance[minimum_distance > 0],bins=[100,20],cmap=plt.get_cmap('jet'))
+    plt.title('AURKA %s minimum %s salt bridge distance over time %s' % (mutant['RUN%s' % run], bridge, system[project]))
+    plt.ylabel('distance r (nanometers) between residues %s and %s' % (bridge.split('-')[0], bridge.split('-')[1]))
+    plt.xlabel('t (nanoseconds)')
+    plt.colorbar()
+    plt.axis([0,500,0.25,0.65])
+    plt.savefig("./plots/AURKA-salt-bridge-%s-hist2d-entire-traj-%s-RUN%s" % (bridge, project, run),dpi=300)
+    plt.close(fig1)
 
 for i, project in enumerate(projects):
     project_dir = project_dirs[project]
     for run in range(5):
-        if not os.path.exists('%s/data/%s_%s_SB_total.npy' % (project_dir, project, run)):
-            continue
-        SB_total = np.load('%s/data/%s_%s_SB_total.npy' % (project_dir, project, run))
+        for bridge in ['181-185','181-162']:
+            if not os.path.exists('%s/data/%s_%s_%s_SB_total.npy' % (project_dir, project, run, bridge)):
+                continue
+            SB_total = np.load('%s/data/%s_%s_%s_SB_total.npy' % (project_dir, project, run, bridge))
 
-        minimum_distance = np.zeros((50,2000))
-        x_axis = np.zeros((50,2000))
-        for clone, traj in enumerate(SB_total):
-            for index in range(2000):
-                try:
-                    minimum_distance[clone][index] = traj[index]
-                except:
-                    pass
-                x_axis[clone][index] = index*0.25
-        x_axis = x_axis.flatten()
-        minimum_distance = minimum_distance.flatten()
-        
-        fig1 = plt.figure()
-        plt.hist2d(x_axis[minimum_distance > 0],minimum_distance[minimum_distance > 0],bins=[100,20],cmap=plt.get_cmap('jet'))
-        plt.title('AURKA %s minimum salt bridge distance over time %s' % (mutant['RUN%s' % run], system[project]))
-        plt.ylabel('distance r (nanometers) between residues 181 and 185')
-        plt.xlabel('t (nanoseconds)')
-        plt.colorbar()
-        plt.axis([0,500,0.25,0.65])
-        plt.savefig("./plots/AURKA-salt-bridge-hist2d-entire-traj-%s-RUN%s" % (project, run),dpi=300)
-        plt.close(fig1)
+            minimum_distance = np.zeros((50,2000))
+            x_axis = np.zeros((50,2000))
+            for clone, traj in enumerate(SB_total):
+                for index in range(2000):
+                    try:
+                        minimum_distance[clone][index] = traj[index]
+                    except:
+                        pass
+                    x_axis[clone][index] = index*0.25
+            x_axis = x_axis.flatten()
+            minimum_distance = minimum_distance.flatten()
+            plot_2dhist(bridge, x_axis, minimum_distance, run, project)
