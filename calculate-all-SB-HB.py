fdade4c33909be5e2e701d180fd92189b5a860ed
juliebehr <jul.marie.behr@gmail.com>
2016-02-25 23:41:00 -0500

    Calculation script, using sidechains or backbones of residues in hbonds where appropriate

--- calculate-all-SB-HB.py
+++ calculate-all-SB-HB.py
@@ -21,6 +21,35 @@ for entry in run_index.split('\n'):
     except:
         pass
 
+def find_hbonds_for_this_traj(traj, residue, haystack, sidechain=False, backbone=False):
+    if sidechain:
+        residue_atoms = [atom.index for atom in residue.atoms if atom.is_sidechain]
+    elif backbone:
+        residue_atoms = [atom.index for atom in residue.atoms if atom.is_backbone]
+    else:
+        residue_atoms = [atom.index for atom in residue.atoms]
+    neighbor_set = find_neighbor_set(traj, residue, haystack)
+    hbonds0 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=residue_atoms, proposed_acceptor_indices=neighbor_set)
+    hbonds1 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=neighbor_set, proposed_acceptor_indices=residue_atoms)
+    hbonds = list()
+    for frame, bondlist in enumerate(hbonds0):
+        try:
+            hbonds.append(np.concatenate((bondlist,hbonds1[frame])))
+        except Exception as e:
+            print('hbonds0')
+            print(bondlist)
+            print(bondlist.shape)
+            print('hbonds1')
+            print(hbonds1[frame])
+            print(hbonds1[frame].shape)
+            raise(e)
+    return hbonds
+
+def find_neighbor_set(traj, residue, haystack):
+    neighbors = md.compute_neighbors(traj, 0.3, residue, haystack_indices=haystack)
+    neighbor_set = set(chain.from_iterable(neighbors))
+    return list(neighbor_set)
+
 try:
     this_run = int(sys.argv[1]) # 1 - 10
 except:
@@ -31,16 +60,17 @@ if this_run is not None:
     runs = [runs[this_run%5]]
 
 
-verbose = False
+verbose = True
 for project in projects:
     project_dir = project_dirs[project]
     for run in runs:
         if verbose:
-            print("Loading RUN%s..." % run)
+            print("Loading Project %s RUN%s..." % (project, run))
         SB_eq_total = []
         SB_ek_total = []
         HB_185_total = []
         HB_181_total = []
+        HB_274_total = []
         HB_275_total = []
         trajectories = dataset.MDTrajDataset("/cbio/jclab/projects/fah/fah-data/munged2/all-atoms/%s/run%d-clone*.h5" % (project, run))
         for i,traj in enumerate(trajectories):
@@ -52,6 +82,8 @@ for project in projects:
                         q185 = residue
                     if str(residue) == 'LYS162':
                         k162 = residue
+                    if str(residue) == 'ASP274':
+                        d274 = residue
                     if str(residue) == 'PHE275':
                         f275 = residue
             res185 = traj.topology.residue(q185.index)
@@ -61,70 +93,20 @@ for project in projects:
             SB_eq_total.append(distances_181_185[:,0])
             SB_ek_total.append(distances_181_162[:,0])
 
-            res185atoms = [atom.index for atom in res185.atoms]
             haystack = traj.top.select("water")
-            neighbors = md.compute_neighbors(traj, 0.3, res185atoms, haystack_indices=haystack)
 
-            neighbor_set = set(chain.from_iterable(neighbors))
-            neighbor_set = list(neighbor_set)
-            np.save('%s/data/%s_RUN%s_clone%s_water_indices.npy' % (project_dir, project, run, i), neighbor_set)
-
-            # using wernet_nilsson because the output is hbonds per frame
-            hbonds0 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=res185atoms, proposed_acceptor_indices=neighbor_set)
-            hbonds1 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=neighbor_set, proposed_acceptor_indices=res185atoms)
-            hbonds = list()
-            for frame, bondlist in enumerate(hbonds0):
-                try:
-                    hbonds.append(np.concatenate((bondlist,hbonds1[frame])))
-                except Exception as e:
-                    print('hbonds0')
-                    print(bondlist)
-                    print(bondlist.shape)
-                    print('hbonds1')
-                    print(hbonds1[frame])
-                    print(hbonds1[frame].shape)
-                    raise(e)
-            HB_185_total.append(hbonds)
-
-            res181atoms = [atom.index for atom in e181.atoms]
-            hbonds0 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=res181atoms, proposed_acceptor_indices=neighbor_set)
-            hbonds1 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=neighbor_set, proposed_acceptor_indices=res181atoms)
-            hbonds = list()
-            for frame, bondlist in enumerate(hbonds0):
-                try:
-                    hbonds.append(np.concatenate((bondlist,hbonds1[frame])))
-                except Exception as e:
-                    print('hbonds0')
-                    print(bondlist)
-                    print(bondlist.shape)
-                    print('hbonds1')
-                    print(hbonds1[frame])
-                    print(hbonds1[frame].shape)
-                    raise(e)
-            HB_181_total.append(hbonds)
-
-            res275atoms = [atom.index for atom in f275.atoms]
-            hbonds0 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=res275atoms, proposed_acceptor_indices=neighbor_set)
-            hbonds1 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=neighbor_set, proposed_acceptor_indices=res275atoms)
-            hbonds = list()
-            for frame, bondlist in enumerate(hbonds0):
-                try:
-                    hbonds.append(np.concatenate((bondlist,hbonds1[frame])))
-                except Exception as e:
-                    print('hbonds0')
-                    print(bondlist)
-                    print(bondlist.shape)
-                    print('hbonds1')
-                    print(hbonds1[frame])
-                    print(hbonds1[frame].shape)
-                    raise(e)
-            HB_275_total.append(hbonds)
+            HB_185_total.append(find_hbonds_for_this_traj(traj, res185, haystack, sidechain=True))
+            HB_181_total.append(find_hbonds_for_this_traj(traj, e181, haystack, sidechain=True))
+            HB_274_total.append(find_hbonds_for_this_traj(traj, d274, haystack, backbone=True))
+            HB_275_total.append(find_hbonds_for_this_traj(traj, f275, haystack, backbone=True)
 
 
         np.save('%s/data/%s_%s_181_HBonds.npy' % (project_dir, project, run), HB_181_total)
         np.save('%s/data/%s_%s_185_HBonds.npy' % (project_dir, project, run), HB_185_total)
+        np.save('%s/data/%s_%s_274_HBonds.npy' % (project_dir, project, run), HB_274_total)
         np.save('%s/data/%s_%s_275_HBonds.npy' % (project_dir, project, run), HB_275_total)
         np.save('%s/data/%s_%s_181-185_SB_total.npy' % (project_dir, project, run), SB_eq_total)
         np.save('%s/data/%s_%s_181-162_SB_total.npy' % (project_dir, project, run), SB_ek_total)
 
-
+if verbose:
+    print('Complete!')
