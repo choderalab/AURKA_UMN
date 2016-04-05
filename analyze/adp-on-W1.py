70807ed2e0e387e5aa1314d6a3aed991d7b5699a
juliebehr <jul.marie.behr@gmail.com>
2016-04-05 12:16:33 -0400

    find water bonds with ADP to filter W1

---
+++ analyze/adp-on-W1.py
@@ -0,0 +1,86 @@
+"""
+Create true/false filter for whether ADP is bound in a given frame
+"""
+import numpy as np
+import sys
+import math
+import os
+import mdtraj as md
+from msmbuilder import dataset
+from itertools import chain
+
+projects = ['11410','11411']
+project_dirs = {'11410':'/cbio/jclab/projects/behrj/AURKA_UMN/output-1OL5','11411':'/cbio/jclab/projects/behrj/AURKA_UMN/output-1OL7'}
+system = {'11410':'with TPX2','11411':'without TPX2'}
+runs = range(5)
+
+with open('/cbio/jclab/projects/behrj/AURKA_UMN/output-1OL7/run-index.txt','r') as fi:
+    run_index = fi.read()
+
+mutant = dict()
+for entry in run_index.split('\n'):
+    try:
+        mutant[entry.split(' ')[0]] = entry.split(' ')[1]
+    except:
+        pass
+
+def find_hbonds_for_this_traj(traj, residue, haystack, sidechain=False, backbone=False):
+    if sidechain:
+        residue_atoms = [atom.index for atom in residue.atoms if atom.is_sidechain]
+    elif backbone:
+        residue_atoms = [atom.index for atom in residue.atoms if atom.is_backbone]
+    else:
+        residue_atoms = [atom.index for atom in residue.atoms]
+    neighbor_set = find_neighbor_set(traj, residue_atoms, haystack)
+    if verbose:
+        print('Finding hbonds for run%s clone%s' % (run, i))
+    hbonds0 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=residue_atoms, proposed_acceptor_indices=neighbor_set)
+    hbonds1 = md.wernet_nilsson(traj, exclude_water=False, proposed_donor_indices=neighbor_set, proposed_acceptor_indices=residue_atoms)
+    hbonds = list()
+    for frame, bondlist in enumerate(hbonds0):
+        try:
+            hbonds.append(np.concatenate((bondlist,hbonds1[frame])))
+        except Exception as e:
+            print('hbonds0')
+            print(bondlist)
+            print(bondlist.shape)
+            print('hbonds1')
+            print(hbonds1[frame])
+            print(hbonds1[frame].shape)
+            raise(e)
+    return hbonds
+
+def find_neighbor_set(traj, residue, haystack):
+    neighbors = md.compute_neighbors(traj, 0.4, residue, haystack_indices=haystack)
+    neighbor_set = set(chain.from_iterable(neighbors))
+    return list(neighbor_set)
+
+try:
+    this_project = int(sys.argv[1]) # 1 - 10
+except:
+    this_project = None
+
+if this_project is not None:
+    projects = [projects[this_project%2]]
+
+verbose = True
+for project in projects:
+    project_dir = project_dirs[project]
+    for run in runs:
+        HB_adp_water = list()
+        if verbose:
+            print("Loading Project %s RUN%s..." % (project, run))
+        trajectories = dataset.MDTrajDataset("/cbio/jclab/projects/fah/fah-data/munged2/all-atoms/%s/run%d-clone*.h5" % (project, run))
+        for i,traj in enumerate(trajectories):
+            if i == 0:
+                for residue in traj.topology.residues:
+                    if str(residue).startswith('MOL') or str(residue).startswith('ADP'):
+                        adp = residue
+                        break
+                haystack = traj.top.select("water")
+            HB_adp_water.append(find_hbonds_for_this_traj(traj, adp, haystack))
+        np.save('%s/data/%s_%s_adp-hbond-water.npy' % (project_dir, project, run), HB_adp_water)
+
+
+if verbose:
+    print('Complete!')
