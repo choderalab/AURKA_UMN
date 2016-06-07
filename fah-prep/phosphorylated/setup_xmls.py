edb2692a66ed0ab070a9566dd505261e832438d7
Julie Behr <julie.behr@choderalab.org>
2016-06-07 19:15:30 -0400

    define TPO bonds each mutant

--- fah-prep/phosphorylated/setup_xmls.py
+++ fah-prep/phosphorylated/setup_xmls.py
@@ -22,6 +22,29 @@ import mdtraj as md
 
 #
 
+def tpo_bonds(modeller, residue):
+    for bond in modeller.topology.bonds():
+        if residue in [bond[0].residue, bond[1].residue]:
+            modeller.topology._bonds = list(filter(lambda a: a != bond, modeller.topology._bonds))
+    bonds = list()
+    bonds.append(("N","CA"))
+    bonds.append(("CA","C"))
+    bonds.append(("CA","CB"))
+    bonds.append(("CB","OG1"))
+    bonds.append(("CB","CG2"))
+    bonds.append(("OG1","P"))
+    bonds.append(("P","O3P"))
+    bonds.append(("P","O2P"))
+    bonds.append(("P","O1P"))
+    bonds.append(("C","O"))
+    name_dict = dict()
+    for atom in residue.atoms():
+        name_dict[atom.name] = atom
+    for bond in bonds:
+        modeller.topology.addBond(name_dict[bond[0]],name_dict[bond[1]])
+    return modeller
+
+
 TPX2 = True
 
 if TPX2:
@@ -142,6 +165,17 @@ residues = set(residues)
 exception_filename = os.path.join(output_path, 'exceptions.out') # to store exceptions
 run_index_filename = os.path.join(output_path, 'run-index.txt') # to store index of which mutants are which
 
+existing_mutants = list()
+if os.path.exists(run_index_filename):
+    infile = open(run_index_filename, 'r')
+    lines = infile.readlines()
+    for line in lines:
+        [run_name, name] = line.strip().split()
+        existing_mutants.append(name)
+    infile.close()
+print "Existing mutants:"
+print existing_mutants
+
 npoint_mutants = len(point_mutants)
 
 mutant_names = list()
@@ -172,6 +206,10 @@ run_index_outfile = open(run_index_filename, 'a')
 runs = 0
 for (name, mutant) in zip(mutant_names, mutant_codes):
     print "%s : %s" % (name, str(mutant))
+    if name in existing_mutants:
+        # Skip this.
+        print "%s : %s already exists, skipping" % (name, str(mutant))        
+        continue
     simulation = None
     if True:
 #    try:
@@ -187,17 +225,17 @@ for (name, mutant) in zip(mutant_names, mutant_codes):
                 print e
                 exception_outfile.write("%s : %s" % (name, str(mutant)) + '\n')
                 exception_outfile.write(str(e) + '\n')
-                continue
+                raise(e)
 
             #fixer.topology.createStandardBonds()
             fixer.missingResidues = {}
-            #print "findMissingAtoms..."
+            print "findMissingAtoms..."
             fixer.findMissingAtoms()
             print(fixer.missingAtoms)
             #print "addMissingAtoms..."
             fixer.addMissingAtoms()
-            print "addingmissinghydrogens..."
-            fixer.addMissingHydrogens(pH)
+#            print "addingmissinghydrogens..."
+#            fixer.addMissingHydrogens(pH)
             #fixer.addSolvent(fixer.topology.getUnitCellDimensions())
 
         # Create directory to store files in.
@@ -205,6 +243,10 @@ for (name, mutant) in zip(mutant_names, mutant_codes):
         if not os.path.exists(workdir):
             os.makedirs(workdir)
             print "Creating path %s" % workdir
+
+
+        filename = os.path.join(workdir, "freshmut.pdb")
+        app.PDBFile.writeFile(fixer.topology, fixer.positions, open(filename, 'w'), keepIds=True)
 
         # Create Modeller
         if verbose: print "Loading forcefield..."
@@ -214,6 +256,43 @@ for (name, mutant) in zip(mutant_names, mutant_codes):
         for residue in modeller.topology.residues():
             if residue.name == 'ADP':
                 modeller.delete([residue])
+        if len(mutant) > 0:
+            for residue in modeller.topology.residues():
+                if residue.name == 'TPO':
+                    tpo1 = residue
+                    break
+            for residue in modeller.topology.residues():
+                if residue.index == tpo1.index-1:
+                    prev = residue
+                elif residue.index == tpo1.index+1:
+                    tpo2 = residue
+                elif residue.index == tpo1.index+2:
+                    follow = residue
+                    break
+            for atom in prev.atoms():
+                if atom.name == 'C':
+                    prevC = atom
+                    break
+            for atom in tpo1.atoms():
+                if atom.name == 'N':
+                    tpo1N = atom
+                elif atom.name == 'C':
+                    tpo1C = atom
+            for atom in tpo2.atoms():
+                if atom.name == 'N':
+                    tpo2N = atom
+                elif atom.name == 'C':
+                    tpo2C = atom
+            for atom in follow.atoms():
+                if atom.name == 'N':
+                    followN = atom
+                    break
+            modeller = tpo_bonds(modeller, tpo1)
+            modeller = tpo_bonds(modeller, tpo2)
+            modeller.topology.addBond(prevC, tpo1N)
+            modeller.topology.addBond(tpo1C, tpo2N)
+            modeller.topology.addBond(tpo2C, followN)
+
         modeller.loadHydrogenDefinitions("h-TPO.xml")
         modeller.addHydrogens(forcefield=forcefield,pH=pH)
         if verbose: print("Writing protonated solute")
@@ -238,7 +317,7 @@ for (name, mutant) in zip(mutant_names, mutant_codes):
 
         # Create OpenMM system.
         if verbose: print "Creating OpenMM system..."
-        #system = forcefield.createSystem(modeller.topology, nonbondedMethod=nonbonded_method, nonbondedCutoff=nonbonded_cutoff, constraints=app.HBonds)
+        #system = forcefield.createSystem(modeller.topology, nonbondedMethod=nonbonded_method, nonbondedCutoff=nonbonded_cutoff, constraints=app.HAngles)
         system = forcefield.createSystem(modeller.topology, nonbondedMethod=nonbonded_method, nonbondedCutoff=nonbonded_cutoff, constraints=None)
         if verbose: print "Adding barostat..."
         system.addForce(openmm.MonteCarloBarostat(pressure, temperature, barostat_frequency))
@@ -268,6 +347,11 @@ for (name, mutant) in zip(mutant_names, mutant_codes):
         positions = simulation.context.getState(getPositions=True).getPositions(asNumpy=True)
         modeller = app.Modeller(simulation.topology, positions)
 
+        # Write modeller positions.
+        if verbose: print("Writing modeller output...")
+        filename = os.path.join(workdir, 'minimize-1.pdb')
+        app.PDBFile.writeFile(modeller.topology, modeller.positions, open(filename, 'w'), keepIds=True)
+
         del(system)
         del(integrator)
         del(platform)
@@ -288,12 +372,6 @@ for (name, mutant) in zip(mutant_names, mutant_codes):
         platform.setPropertyDefaultValue('OpenCLPrecision', 'double') # use double precision
         simulation = app.Simulation(modeller.topology, system, integrator, platform=platform)
         simulation.context.setPositions(modeller.positions)
-
-        # Write modeller positions.
-        if verbose: print "Writing modeller output..."
-        filename = os.path.join(workdir, 'minimize-1.pdb')
-        positions = simulation.context.getState(getPositions=True).getPositions(asNumpy=True)
-        app.PDBFile.writeFile(simulation.topology, positions, open(filename, 'w'), keepIds=True)
 
         # Minimize energy.
         if verbose: print "Minimizing energy..."
@@ -375,25 +453,17 @@ for (name, mutant) in zip(mutant_names, mutant_codes):
             if verbose: print "Final solvated potential energy:  : %10.3f kcal/mol" % (potential_energy / unit.kilocalories_per_mole)
 
 
-        # Write initial positions.
+        # Write minimized positions.
         filename = os.path.join(workdir, 'minimized.pdb')
         positions = simulation.context.getState(getPositions=True).getPositions()
         app.PDBFile.writeFile(simulation.topology, positions, open(filename, 'w'), keepIds=True)
 
-        if verbose: print "Taking a few steps..."
-        temp_steps = [100,50,30,15,10,100,50,30,15,10,6,4,100,50,30,15,10,6,4,3,2,1]
-        for j in temp_steps:
-            # Assign temperature
-            temp = temperature / j
-            print("Set temp to %s K" % temp)
-            simulation.context.setVelocitiesToTemperature(temp)
-            # Take a few steps to relax structure.
-            for k in range(nsteps/len(temp_steps)+1):
-                simulation.step(1)
-                filename = os.path.join(workdir, 'stepping-'+str(j)+'-'+str(k)+'.pdb')
-                positions = simulation.context.getState(getPositions=True).getPositions()
-                app.PDBFile.writeFile(simulation.topology, positions, open(filename, 'w'), keepIds=True)
+        # Assign temperature
+        simulation.context.setVelocitiesToTemperature(temperature)
 
+        # Take a few steps to relax structure.
+        if verbose: print "Taking a few steps..."
+        simulation.step(nsteps)
 
         # Write initial positions.
         if verbose: print "Writing positions..."
