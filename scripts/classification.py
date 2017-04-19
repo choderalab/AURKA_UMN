ba67427677c33c0177963c87415f5ebb2f5b4de0
steven-albanese <steven.albanese91@gmail.com>
2017-04-18 20:56:58 -0400

    getting rid of plotting

--- scripts/classification.py
+++ scripts/classification.py
@@ -1,7 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import mdtraj as md
-plt.rc('font',family='serif')
 import re
 from sklearn import preprocessing
 from sklearn.tree import DecisionTreeClassifier
@@ -63,17 +61,6 @@ def FeaturizeContactsAndClassify():
 
     return [X, Y, le, feat]
 
-def plot():
-    ax = plt.gca()
-    # how does the accuracy increase as we include more features
-    plt.plot(np.cumsum(sorted(clf.feature_importances_[clf.feature_importances_ != 0])[::-1]) * clf.score(X, y), marker='o')
-    plt.hlines(dt.score(X,y),0,sum(dt.feature_importances_!=0),linestyles='--')
-    plt.xlabel('# of Contacts used')
-    plt.ylabel('Fraction of state identities explained')
-    plt.title('Discriminating AURKA Phosphorylation State')
-    plt.savefig('id_explained_contacts.pdf', dpi=500)
-    plt.close()
-
 if __name__ == "__main__":
     fnames_phos = glob('/cbio/jclab/home/albaness/trajectories/AURKA/AURKA_phos_notpx2/*/run0-*.h5')
     print(len(fnames_phos), fnames_phos[0])
@@ -83,24 +70,3 @@ if __name__ == "__main__":
     [X, y, le, feat] = FeaturizeContactsAndClassify()
     np.save('distances.npy', X)
     np.save('run_labels_dist.npy', y)
-    clf = RandomForestClassifier(n_estimators=10, max_depth=None,
-                                 min_samples_split=2, random_state=0)
-    clf = clf.fit(X, y)
-    scores = cross_val_score(clf, X, y)
-    print(scores)
-    print(clf.score(X, y))
-    print(clf.feature_importances_)
-    plot()
-    print(len(clf.feature_importances_[clf.feature_importances_ != 0]))
-    sorted_importance = sorted(clf.feature_importances_[clf.feature_importances_!=0])
-    sorted_non_zero_indices = []
-    for value in sorted_importance:
-        for i, x in enumerate(clf.feature_importances_):
-            if x == value:
-                sorted_non_zero_indices.append(i)
-            else:
-                pass
-
-    for index in sorted_non_zero_indices[:100]:
-        print(feat.describe()[index])
-        print(clf.feature_importances_[index])
