38aa9b6d5b049feb24fd4f546c0b0fbee1e45e9e
steven-albanese <steven.albanese91@gmail.com>
2017-04-24 14:05:35 -0400

    fixing another typoe

--- scripts/distances.py
+++ scripts/distances.py
@@ -59,13 +59,13 @@ if __name__ == "__main__":
     trajectories = dataset.MDTrajDataset(
         '/cbio/jclab/home/albaness/trajectories/AURKA/%s/*/run0-*.h5' % condition)
     master_dist_list = []
-    for pair in range(len(res_pairs)):
+    for pair in range(len(res_pairs[kinase_definition])):
         dist_list = []
-        pair_id = res_pairs[pair]
-        res1 = pair_id[0]
-        res2 = pair_id[1]
-        print(pair_id)
+        pair_list = res_pairs[kinase_definition][pair]
+        res1 = pair_list[0]
+        res2 = pair_list[1]
+        print(pair_list)
         for traj_in in trajectories:
-            distance1 = alpha_distances(traj_in, res_pairs[pair])
+            distance1 = alpha_distances(traj_in, pair_list)
             dist_list.extend(distance1)
         np.save('../data/probe_analysis/distances_%s_%s-pair%s-%s.npy' % (kinase_definition, condition, res1, res2))
