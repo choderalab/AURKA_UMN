d484363d336e399824895bf852c799dd9bb664a9
John Chodera <choderaj@mskcc.org>
2017-07-29 23:57:53 -0400

    Add diagnostics of activation loop helical formation

--- spin-probe-analysis/README.md
+++ spin-probe-analysis/README.md
@@ -11,8 +11,25 @@ This section contains analysis scripts and processed datafiles for simulations w
   * index 2 : R255 CZ - T288 CA distance
   * index 3 : F275 CZ - I193 CG2 distance (low in DFG-in)
   * index 4 : W277 CE2 - I193 CG2 distance (low in DFG-out)
+  * index 5 : P282 O - R285 H distance (activation loop helical contact)
+  * index 6 : S283 O - R286 H distance (activation loop helical contact)
+  * index 7 : L225 CA - S284 CA distance (CYR-CYR here)
+  * index 8 : Pseudotorsion for residues 282-285
+  * index 9 : Pseudotorsion for residues 283-286
+
 * `Analysis of spin probe distances.ipynb` - Jupyter notebook for analysis of distances
 * `Visualize.ipynb` - [EXPERIMENTAL] - Jupyter notebook to fetch trajectories and visualize them with `nglview`
+
+## Running `compute-distances.py`
+
+Start a job on `hal`:
+```bash
+qsub -I -l walltime=04:00:00,nodes=1:ppn=12 -l mem=32G -q active
+```
+Run the script:
+```bash
+python compute-distances.py
+```
 
 ## Run index
 
--- spin-probe-analysis/compute-distances.py
+++ spin-probe-analysis/compute-distances.py
@@ -62,35 +62,63 @@ def process_clone(clone_path):
     # Determine spin probe NO oxygen atom indices
     oxygens = traj.top.select('resn CYR and name NN')
     if len(oxygens) != 2:
-        raise Exception('Selection for spin probe NN-NN distance did not return exactly two atoms')
+        raise Exception('%s: Selection for spin probe NN-NN distance did not return exactly two atoms' % clone_path)
 
     # Determine spin probe CA distances
     alpha_carbons = traj.top.select('resn CYR and name CA')
     if len(alpha_carbons) != 2:
-        raise Exception('Selection for spin probe CA-CA distance did not return exactly two atoms')
+        raise Exception('%s: Selection for spin probe CA-CA distance did not return exactly two atoms' % clone_path)
 
     # Determine R255 CZ - T288 CA distance
     RT = traj.top.select('(resSeq %d and resname ARG and name CZ) or (resSeq %d and resname THR and name CA)' % (255 - offset + 1, 288 - offset + 1))
     if len(RT) != 2:
-        raise Exception('Selection for R255 CZ - T288 CA distance did not return exactly two atoms')
+        raise Exception('%s: Selection for R255 CZ - T288 CA distance did not return exactly two atoms' % clone_path)
 
     # Determine F275 CZ - I193 CG2 distance
     DFG_PHE_ILE = traj.top.select('(resSeq %d and resname PHE and name CZ) or (resSeq %d and resname ILE and name CG2)' % (275 - offset + 1, 193 - offset + 1))
     if len(DFG_PHE_ILE) != 2:
-        raise Exception('Selection for F275 CZ - I193 CG2 distance did not return exactly two atoms')
+        raise Exception('%s: Selection for F275 CZ - I193 CG2 distance did not return exactly two atoms' % clone_path)
 
     # Determine W277 CE2 - I193 CG2 distance
     DFG_TRP_ILE = traj.top.select('(resSeq %d and resname TRP and name CE2) or (resSeq %d and resname ILE and name CG2)' % (277 - offset + 1, 193 - offset + 1))
     if len(DFG_TRP_ILE) != 2:
-        raise Exception('Selection for W277 CE2 - I193 CG2 distance did not return exactly two atoms')
+        raise Exception('%s: Selection for W277 CE2 - I193 CG2 distance did not return exactly two atoms' % clone_path)
+
+    # Determine P282 O - R285 H distance (activation loop helical contact)
+    helix_hbonds_1 = traj.top.select('(resSeq %d and resname PRO and name O) or (resSeq %d and resname ARG and name H)' % (282 - offset + 1, 285 - offset + 1))
+    if len(helix_hbonds_1) != 2:
+        raise Exception('%s: Selection for P282 O - R285 HN distance did not return exactly two atoms (%d instead)' % (clone_path, len(helix_hbonds_1)))
+
+    # Determine S283 O - R286 H distance (activation loop helical contact)
+    helix_hbonds_2 = traj.top.select('(resSeq %d and resname SER and name O) or (resSeq %d and resname ARG and name H)' % (283 - offset + 1, 286 - offset + 1))
+    if len(helix_hbonds_2) != 2:
+        raise Exception('%s: Selection for S283 O - R286 HN distance did not return exactly two atoms (%d instead)' % (clone_path, len(helix_hbonds_2)))
+
+    # Determine L225 CA - S284 CA distance (CYR-CYR here)
+    #L225_S284 = traj.top.select('(resSeq %d and resname LEU and name CA) or (resSeq %d and resname SER and name CA)' % (225 - offset + 1, 284 - offset + 1))
+    L225_S284 = traj.top.select('(resSeq %d and resname CYR and name CA) or (resSeq %d and resname CYR and name CA)' % (225 - offset + 1, 284 - offset + 1))
+    if len(L225_S284) != 2:
+        raise Exception('%s: Selection for L225 CA -  S284 CA distance did not return exactly two atoms (%d instead)' % (clone_path, len(L225_S284)))
 
     # Compute distances
-    distances = md.compute_distances(traj, [oxygens, alpha_carbons, RT, DFG_PHE_ILE, DFG_TRP_ILE])
+    distances = md.compute_distances(traj, [oxygens, alpha_carbons, RT, DFG_PHE_ILE, DFG_TRP_ILE, helix_hbonds_1, helix_hbonds_2, L225_S284])
+
+    # Determine pseudotorsion for residues 282-285
+    r282ca = traj.top.select('(resSeq %d and name CA)' % (282 - offset + 1,))[0]
+    r283ca = traj.top.select('(resSeq %d and name CA)' % (283 - offset + 1,))[0]
+    r284ca = traj.top.select('(resSeq %d and name CA)' % (284 - offset + 1,))[0]
+    r285ca = traj.top.select('(resSeq %d and name CA)' % (285 - offset + 1,))[0]
+    r286ca = traj.top.select('(resSeq %d and name CA)' % (286 - offset + 1,))[0]
+    torsions = md.compute_dihedrals(traj, np.array([
+                [r282ca, r283ca, r284ca, r285ca],
+                [r283ca, r284ca, r285ca, r286ca]]))                                                                     
+
+    data = np.concatenate((distances, torsions), axis=1)
 
     # Clean up
     del traj
-    
-    return distances
+                                    
+    return data
 
 if __name__ == '__main__':
     from multiprocessing import Pool
