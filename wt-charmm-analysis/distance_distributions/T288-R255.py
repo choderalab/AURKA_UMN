d44e10de140f6ddd6f33235b93998adfb15976c3
steven-albanese <steven.albanese91@gmail.com>
2017-12-12 18:10:51 -0500

    checking in distance analysis

---
+++ wt-charmm-analysis/distance_distributions/T288-R255.py
@@ -0,0 +1,60 @@
+# A script to compute the fraction of a given contact in all of your frames + the standard error
+
+import sys
+import mdtraj as md
+import numpy as np
+from msmbuilder import dataset
+
+
+# Define condition.
+condition = sys.argv[1]
+
+# Define kinase offset.
+offset = sys.arv[2]
+
+# Define Alpha carbon coordinates (1-indexed), give me residue numbers here
+res_pairs = {'AURKA': [[288, 255]]}
+
+
+
+
+def alpha_distances(traj, residue_pair):
+    """
+    :param traj: the trajectory to be analyzed
+    :param spine: the residues involved
+    :return: two flattened numpy arrays
+    """
+    min_frame = 400
+    end_frame = len(traj)
+    short_traj = traj.slice(range(min_frame, end_frame), copy=False)
+    atom1 = short_traj.topology.select("residue %s and name == 'CA'" % residue_pair[0])
+    atom2 = short_traj.topology.select("residue %s and name == 'CZ'" % residue_pair[1])
+    list_of_atoms = [atom1, atom2]
+    atom_array = np.asanyarray(list_of_atoms)
+    atom_array = atom_array.reshape(1,2)
+    dists = md.compute_distances(short_traj, atom_array)
+
+
+
+    # Append difference and individual distances
+    dists = np.multiply(dists, 10)
+    dists = dists.flatten()
+
+    # flatten list of arrays
+    return dists
+
+
+if __name__ == "__main__":
+    trajectories = dataset.MDTrajDataset(
+        '/cbio/jclab/home/albaness/trajectories2/AURKA/CHARMM/%s/*.h5' % condition)
+    master_dist_list = []
+    for pair in range(len(res_pairs[kinase_definition])):
+        dist_list = []
+        pair_list = res_pairs[kinase_definition][pair]
+        res1 = pair_list[0] - offset
+        res2 = pair_list[1] - offset
+        print(pair_list)
+        for traj_in in trajectories:
+            distance1 = alpha_distances(traj_in, pair_list)
+            dist_list.extend(distance1)
+        np.save('../data/distances/distances_CHARMM_AURKA_%s-pair%s-%s.npy' % (condition, res1, res2), dist_list)
---
+++ wt-charmm-analysis/distance_distributions/analyze_distances.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# walltime : maximum wall clock time (hh:mm:ss)
+#PBS -l walltime=144:00:00
+#
+# join stdout and stderr
+#PBS -j oe
+#
+# spool output immediately
+#PBS -k oe
+#
+# specify queue
+#PBS -q batch
+#
+# nodes: number of nodes
+#   ppn: number of processes per node
+#PBS -l nodes=1:ppn=1
+#
+# specify memory
+#
+#PBS -l mem=80GB
+#
+# export all my environment variables to the job
+#PBS -V
+#
+# job name (default = name of script file)
+#PBS -N CHARMM_distances
+#
+# specify email for notifications
+#PBS -M steven.albanese@choderalab.org
+#
+# mail settings (one or more characters)
+# n: do not send mail
+# a: send mail if job is aborted
+# b: send mail when job begins execution
+# e: send mail when job terminates
+#PBS -m ae
+#
+# filename for standard output (default = <job_name>.o<job_id>)
+# at end of job, it is in directory from which qsub was executed
+# remove extra ## from the line below if you want to name your own file
+#PBS -o AURKA_CHARMM_DIST
+
+# Change to working directory used for job submission
+cd $PBS_O_WORKDIR
+#source activate py27
+
+
+python ./L225-S284.py phos_tpx2 122
+python ./L225-S284.py nophos_tpx2 122
+python ./L225-S284.py phos_notpx2 122
+python ./L225-S284.py nophos_notpx2 122
+
+python ./T288-A167.py phos_tpx2 122
+python ./T288-A167.py nophos_tpx2 122
+python ./T288-A167.py phos_notpx2 122
+python ./T288-A167.py nophos_notpx2 122
+
+python ./T288-R180.py phos_tpx2 122
+python ./T288-R180.py nophos_tpx2 122
+python ./T288-R180.py phos_notpx2 122
+python ./T288-R180.py nophos_notpx2 122
+
+python ./T288-R255.py phos_tpx2 122
+python ./T288-R255.py nophos_tpx2 122
+python ./T288-R255.py phos_notpx2 122
+python ./T288-R255.py nophos_notpx2 122
+
